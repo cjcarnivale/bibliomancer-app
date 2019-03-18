7cecddde44a27d559619f38b093ca074c6561acb
Christopher <christopher.carnivale@gmail.com>
2019-03-18 10:29:06 -0400

    Routes set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+import React, { Component } from 'react';
+import { Route, Switch }from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import LoginPage from './components/LoginPage';  
+import RegistrationPage from './components/RegistrationPage';
+import ProfilePage from './components/ProfilePage'; 
+import NotFoundPage from './components/NotFoundPage'; 
+class App extends Component {
+  render(){
+    return (
+      <main className='App'>
+      <Switch>
+        <Route exact path='/' component={LandingPage}/>
+        <Route path='/LoginPage' component={LoginPage}/>
+        <Route path='/RegistrationPage' component={RegistrationPage}/>
+        <Route path='/ProfilePage' component={ProfilePage}/>
+        <Route component={NotFoundPage}/>
+      </Switch>
+      </main>
+    );
+  }
 }
-
 export default App;
